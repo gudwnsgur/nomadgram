279e277eff6433acde285527ddc71318abc006e5
gudwnsgur <gudwnsgur12@naver.com>
2019-08-14 09:31:43 +0900

    modify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,14 @@ import style from './App.module.scss';
 
 function App() {
   return (
-    <div className={style.app}>
-      <header className={style.appHeader}>
-        <img src={logo} className={style.appLogo} alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className={style.appLink}
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
